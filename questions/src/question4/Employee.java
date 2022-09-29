44ec1cc325a15e07a878db9f48f61d06b5f60244
nikhil999 <nikhil.sharma1@to the new.com>
2022-09-29 10:08:59 +0530

    q4 added

---
+++ questions/src/question4/Employee.java
@@ -0,0 +1,20 @@
+package question4;
+
+public class Employee {
+	String name;
+	int age;
+	String designation;
+	double salary;
+	
+	public Employee(String name,int age,String designation,double salary) {
+		this.name=name;
+		this.age=age;
+		this.designation=designation;
+		this.salary=salary;
+	}
+	
+	
+	
+}
+
+
---
+++ questions/src/question4/main.java
@@ -0,0 +1,20 @@
+package question4;
+import java.util.HashMap;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Employee,Double> mp = new HashMap<Employee,Double>();
+		
+		Employee e1 = new Employee("Mayank",21,"Software Engineer",450000);
+		Employee e2 = new Employee("Sunny",22,"Project Engineer",550000);
+		Employee e3 = new Employee("Ishu",23,"Devops Engineer",650000);
+		
+		mp.put(e1,e1.salary);
+		mp.put(e2,e2.salary);
+		mp.put(e3,e3.salary);
+		System.out.println(mp.get(e3));
+	}
+
+}
+
